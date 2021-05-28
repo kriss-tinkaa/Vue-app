63e343dd4a0a855f5a6bd8610be852221b066e33
kradchenkonlt <k.radchenko@newlinetechnologies.net>
2021-05-28 10:49:15 +0300

    change key

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 firebase.initializeApp({
-    apiKey: "AIzaSyC2BN9jIMWgtrrY_OS7Ll1arsb2kcKoD5s",
-    authDomain: "nn-quiz.firebaseapp.com",
-    projectId: "nn-quiz",
-    storageBucket: "nn-quiz.appspot.com",
-    messagingSenderId: "72670363657",
-    appId: "1:72670363657:web:922efe9d3ca977076878d8",
+    apiKey: "AIzaSyBrzRImoVGmc2wSAUfg8GQdCDH0F4V9yP4",
+    authDomain: "nn-quiz-d19b3.firebaseapp.com",
+    projectId: "nn-quiz-d19b3",
+    storageBucket: "nn-quiz-d19b3.appspot.com",
+    messagingSenderId: "702644996996",
+    appId: "1:702644996996:web:805b650777579929c8ebd1",
     /*databaseURL: "https://nn-quiz.firebaseio.com",*/
 })
 
