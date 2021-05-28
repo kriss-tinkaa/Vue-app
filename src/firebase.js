07c0e9a2813fc36ee79d5781eea16ea2ad188bdd
kradchenkonlt <k.radchenko@newlinetechnologies.net>
2021-05-28 11:02:51 +0300

    add apikey

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,12 @@
 import firebase from 'firebase'
 
 firebase.initializeApp({
+    apiKey: "AIzaSyBrzRImoVGmc2wSAUfg8GQdCDH0F4V9yP4",
+    projectId: "nn-quiz-d19b3",
+    appId: "1:702644996996:web:805b650777579929c8ebd1",
+    messagingSenderId: "702644996996",
+    authDomain: "nn-quiz-d19b3.firebaseapp.com",
+    storageBucket: "nn-quiz-d19b3.appspot.com",
     
 
 
